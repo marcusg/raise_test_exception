ffd3bbc9447c29b5b56f44f1a67f76cfcb3fe2d0
Marcus Geißler <marcus3006@gmail.com>
2013-09-30 21:18:21 +0200

    added helper path specs

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ApplicationHelper, :type => :helper do
 
-  describe "create a reference" do
+  describe "access to route helper methods (url)" do
     it "works with a link" do
       expect(helper.link_to("Raise it!", helper.raise_test_exception_error_url)).to match /Raise it!/
     end
@@ -12,4 +12,16 @@ describe ApplicationHelper, :type => :helper do
     end
 
   end
+
+  describe "access to route helper methods (path)" do
+    it "works with a link" do
+      expect(helper.link_to("Raise it!", helper.raise_test_exception_error_path)).to match /Raise it!/
+    end
+
+    it "works with a button" do
+      expect(helper.button_to("Raise it!", helper.raise_test_exception_error_path)).to match /Raise it!/
+    end
+
+  end
+
 end
